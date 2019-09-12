c2d410c6f89b7550f0b45e6cb845bc96a2e1f769
Trojanzaro <anastasiszaro@gmail.com>
2019-09-11 20:12:10 -0700

    Added keyboard support, almost

--- kernel.c
+++ kernel.c
@@ -33,6 +33,65 @@ size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_colour;
 
+unsigned char kbdus[128] =
+{
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
+  '9', '0', '-', '=', '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'q', 'w', 'e', 'r',	/* 19 */
+  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+ '\'', '`',   0,		/* Left shift */
+ '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+  'm', ',', '.', '/',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};
+
+
+static inline void outb(uint16_t port, uint8_t val)
+{
+    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+    /* There's an outb %al, $imm8  encoding, for compile-time constant port numbers that fit in 8b.  (N constraint).
+     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
+     * The  outb  %al, %dx  encoding is the only option for all other cases.
+     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile ( "inb %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
+
 static inline uint8_t vga_entry_colour(enum vga_colour foreground, enum vga_colour background){
     return foreground | (background << 4);
 }
@@ -103,9 +162,34 @@ void terminal_writeint(unsigned long n){
     terminal_putchar((n % 10) + '0');
 }
 
+void terminal_keyboard_handler(){
+	unsigned char scancode;
+
+    /* Read from the keyboard's data buffer */
+    scancode = inb(0x60);
+
+    /* If the top bit of the byte we read from the keyboard is
+    *  set, that means that a key has just been released */
+    if (inb(0x60) & 0x80 != scancode)
+    {
+	terminal_putchar(kbdus[scancode]);
+        /* You can use this one to see if the user released the
+        *  shift, alt, or control keys... */
+    }
+    else
+    {
+    }
+
+}
+
 void kernel_main() {
 	terminal_initialize();
 	terminal_writestring(welcome_message);
+	terminal_writestring("\n\n");
+	terminal_writestring(">");
+	while(1)
+		terminal_keyboard_handler();
+		
 }
 
 
